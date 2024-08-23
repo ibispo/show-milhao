8068a9319e0da4b3fb7ba9bcb288389869fb21a6
imbispo@gmail.com <imbispo@gmail.com>
2024-08-23 19:32:01 +0100

    Versao inicial

---
+++ src/main/java/com/bispo/filewatch/ShowMilhaoApplication.java
@@ -0,0 +1,13 @@
+package com.bispo.filewatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowMilhaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowMilhaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bispo/filewatch/ShowMilhaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bispo.filewatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowMilhaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
